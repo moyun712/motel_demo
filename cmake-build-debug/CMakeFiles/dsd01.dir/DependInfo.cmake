3a7ac4b4ea8780df93a3f6a2939f9efd009e4c21
蓬蒿 <2723754483@qq.com.com>
2023-12-22 21:09:26 +0800

    冲突测试

---
+++ cmake-build-debug/CMakeFiles/dsd01.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/d/Code/C/model/motel_demo/cmd/menu.c" "CMakeFiles/dsd01.dir/cmd/menu.c.o" "gcc" "CMakeFiles/dsd01.dir/cmd/menu.c.o.d"
+  "/mnt/d/Code/C/model/motel_demo/data/graph_mod_repo.c" "CMakeFiles/dsd01.dir/data/graph_mod_repo.c.o" "gcc" "CMakeFiles/dsd01.dir/data/graph_mod_repo.c.o.d"
+  "/mnt/d/Code/C/model/motel_demo/service/mod.c" "CMakeFiles/dsd01.dir/service/mod.c.o" "gcc" "CMakeFiles/dsd01.dir/service/mod.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
